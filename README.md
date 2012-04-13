88ec4b9bb696179eb08014fed66b73c31762cd90
Vesa Vänskä <vesan2000@gmail.com>
2012-04-13 20:46:58 +0300

    Adds current scripts

---
+++ README.md
@@ -0,0 +1,13 @@
+## Script for OS X
+
+Copy and paste this line the Terminal and press Enter. Enjoy the text flying on the screen, it will take quite some time (approximately 30 minutes). Installer might ask for your administrator password. Grabbing a refreshing drink before starting is encouraged!
+
+    bash < <(curl -s https://raw.github.com/railsgirls/installation-scripts/rails-install-osx.sh)
+
+## Script for Ubuntu
+
+To install Ruby on Rails development environment you just need to copy and paste this line the Terminal and press Enter. Enjoy the text flying on the screen, it will take quite some time. Grabbing a refreshing drink before starting is encouraged.
+
+    bash < <(curl -s  https://raw.github.com/railsgirls/installation-scripts/rails-install-ubuntu.sh)
+
+
---
+++ rails-install-osx.sh
@@ -0,0 +1,40 @@
+set -e
+
+echo "Installs Homebrew for installing other software"
+/usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+brew update
+
+echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
+curl -L get.rvm.io | bash -s stable
+echo "
+# RVM
+[[ -s '/Users/`whoami`/.rvm/scripts/rvm' ]] && source '/Users/`whoami`/.rvm/scripts/rvm'" >> ~/.bash_profile
+source ~/.bash_profile
+
+echo "Install Ruby"
+rvm install 1.9.3-p125
+rvm use 1.9.3-p125 --default
+
+gem install bundler --no-rdoc --no-ri
+gem install rails --no-rdoc --no-ri
+
+echo -e "\n- - - - - -\n"
+echo -e "Now we are going to print some information to check that everything is done:\n"
+
+echo -n "Should be brew 0.8 or higher:       brew "
+brew -v
+echo -n "Should be sqlite 3.7.3 or higher: sqlite "
+sqlite3 --version
+echo -n "Should be rvm 1.6.32 or higher:          "
+rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10
+echo -n "Should be ruby 1.9.3p125:                "
+ruby -v | cut -d " " -f 2
+echo -n "Should be Rails 3.2.2 or higher:         "
+rails -v
+echo -e "\n- - - - - -\n"
+
+echo "If the versions match, everything is installed correctly. If the versions 
+don't match or errors are shown, something went wrong with the automated process 
+and we will help you do the installation the manual way at the event.
+
+Congrats!"
