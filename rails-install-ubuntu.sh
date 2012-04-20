b2f826b3aab39fda9e0976260cd493081f7e06a0
Thilo Utke <thilo@upstre.am>
2012-04-20 15:37:48 +0300

    Also Updated the ubuntu script with latest ruby version.

--- rails-install-ubuntu.sh
+++ rails-install-ubuntu.sh
@@ -14,8 +14,8 @@ curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installs Ruby"
-rvm install 1.9.3-p125
-rvm use 1.9.3-p125 --default
+rvm install 1.9.3-p194
+rvm use 1.9.3-p94 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
@@ -34,7 +34,7 @@ echo -n "Should be sqlite 3.7.3 or higher: sqlite "
 sqlite3 --version
 echo -n "Should be rvm 1.6.32 or higher:          "
 rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10
-echo -n "Should be ruby 1.9.3p125:                "
+echo -n "Should be ruby 1.9.3-p194:                "
 ruby -v | -d " " -f 2
 echo -n "Should be Rails 3.2.2 or higher:         "
 rails -v
