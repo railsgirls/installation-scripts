f4362499e9f041f3c7a4df060e12169c0ecce986
vesan <vesa@vesavanska.com>
2014-06-15 11:31:32 +0300

    Updates installation scripts to install latest Ruby in 1.9.3 branch

--- rails-install-fedora.sh
+++ rails-install-fedora.sh
@@ -17,8 +17,8 @@ curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installs Ruby"
-rvm install 1.9.3-p392
-rvm use 1.9.3-p392 --default
+rvm install 1.9.3-p547
+rvm use 1.9.3-p547 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
@@ -36,7 +36,7 @@ echo -n "Should be sqlite 3.7.3 or higher: sqlite "
 sqlite3 --version
 echo -n "Should be rvm 1.6.32 or higher: "
 rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10 | xargs echo
-echo -n "Should be ruby 1.9.3-p392: "
+echo -n "Should be ruby 1.9.3-p547: "
 ruby -v | cut -d " " -f 2
 echo -n "Should be Rails 3.2.2 or higher: "
 rails -v
--- rails-install-osx-rvm.sh
+++ rails-install-osx-rvm.sh
@@ -15,8 +15,8 @@ curl -kL get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Install Ruby"
-rvm install 1.9.3-p194
-rvm use 1.9.3-p194 --default
+rvm install 1.9.3-p547
+rvm use 1.9.3-p547 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
@@ -32,7 +32,7 @@ echo -n "Should be sqlite 3.7.3 or higher: sqlite "
 sqlite3 --version
 echo -n "Should be rvm 1.6.32 or higher:          "
 rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10
-echo -n "Should be ruby 1.9.3-p194:                "
+echo -n "Should be ruby 1.9.3-p547:                "
 ruby -v | cut -d " " -f 2
 echo -n "Should be Rails 3.2.2 or higher:         "
 rails -v
